79be94ab2163641c8287a5e4839e3a5fd99089e6
misaka2047 <947248080@qq.com>
2024-07-05 15:20:33 +0800

    Site updated: 2024-07-05 15:20:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/05/测试/","2024/07/05/hello-world/"];function toRandomPost(){
+var posts=["2024/07/05/hello-world/","2024/07/05/测试/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
